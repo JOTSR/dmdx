b8d9766de28cb8099c38e548459b97f7e618c69c
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-02-21 11:05:58 +0100

    fix: align cli version to module version

--- cli/dmdx.ts
+++ cli/dmdx.ts
@@ -4,7 +4,7 @@ import { preview } from './preview.ts'
 
 export const cli = new Command()
 	.name('dmdx')
-	.version('0.1.0')
+	.version('0.2.2')
 	.command('transform', transform)
 	.command('preview', preview)
 	.command(
